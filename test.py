8d3f2fbe7e005fda4326f5c43fff034cd9a2e5b7
Nico Jacobs <Nico@u2u.be>
2024-02-20 15:40:38 +0100

    modules

--- test.py
+++ test.py
@@ -1,5 +1,27 @@
 def sumthemallup(**args):
-    for i,v in args.items:
+    for i,v in args.items():
         print(i,v)
 
+class U2UStudent:
+    '''Represents a U2U student'''
+    nrofstudents = 0
+
+    def __init__(self,firstname: str, lastname:str) -> None:
+        '''Provide the official firstname and last name as on the passport'''
+        self._fname = firstname
+        self._lname = lastname
+        U2UStudent.nrofstudents +=1
+
+    @property
+    def FirstName(self):
+        self._askedForName = True
+        return self._fname
+    
+    @FirstName.deleter
+    def FirstName(self):
+        self._fname = "John"
+
+u2uwebsite = "https://www.u2u.be"
+
 sumthemallup(height=10,width=20)
+
